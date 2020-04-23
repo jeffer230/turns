e0478b69f4f2cc1d341700fc8259a8e9c53778d6
jeffer230 <jeffer230@gmail.com>
2020-04-23 13:50:27 -0500

    init commit

--- partials/aside.php
+++
@@ -1,8 +0,0 @@
-<aside class="sidenav">
-	
-	<a href="home.php">Inicio</a>
-	<a href="recordturn.php">Registrar Giro</a>
-	<a href="#">Estado del Giro</a>
-	<a href="#">Registro de mis giros</a>
-
-</aside>
--- partials/header.php
+++
@@ -1,6 +0,0 @@
-<header class="header">
-	<div id="logo">
-		<img src="img/logo.png" alt="logo">	
-	</div>
-	<h3 class="header_title">GIROS TUDINERO</h3>
-</header>
